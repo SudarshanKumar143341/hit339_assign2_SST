1b7dca767c1414b129e7fe29e6761155dd20b7e8
DESKTOP-8CK243U\sudar <sudarshan.bhattarai@students.cdu.edu.au>
2020-09-25 13:16:41 +0930

    added schedulemember.cs partial class for general members

---
+++ TennisForEveryone/Models/ScheduleMembers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TennisForEveryone.Models
+{
+    public partial class ScheduleMembers
+    {
+        [Column("ID")]
+        public int Id { get; set; }
+        [Column("ScheduleID")]
+        public int ScheduleId { get; set; }
+        public string MemberEmail { get; set; }
+    }
+}
