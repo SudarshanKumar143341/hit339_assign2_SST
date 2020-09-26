fac0104f5f8051f996ce85a23292322d95f7d5dc
DESKTOP-8CK243U\sudar <sudarshan.bhattarai@students.cdu.edu.au>
2020-09-26 09:40:33 +0930

    added context for coach, schedule and schedulemembers

--- TennisForEveryone/Data/ApplicationDbContext.cs
+++ TennisForEveryone/Data/ApplicationDbContext.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TennisForEveryone.Models;
 
 namespace TennisForEveryone.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        public virtual DbSet<Coach> Coach { get; set; }
+        public virtual DbSet<Schedule> Schedule { get; set; }
+        public virtual DbSet<ScheduleMembers> ScheduleMembers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+
         }
     }
 }
