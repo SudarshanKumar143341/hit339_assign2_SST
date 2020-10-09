2023d4e43a1e2f95ba2e4fd859dffeaaef63ca97
Sandipa Rijal <sandipa_rizal@hotmail.com>
2020-10-09 10:55:51 +0930

    Update CSS in EditUserInRole.cshtml page

--- TennisForEveryone/Models/ViewModel/RoleViewModel.cs
+++ TennisForEveryone/Models/ViewModel/RoleViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,6 +14,7 @@ namespace TennisForEveryone.Models.ViewModel
         }
         public string Id { get; set; }
 
+        [Required]
         public string RoleName { get; set; }
 
         public List<string> Users { get; set; }
